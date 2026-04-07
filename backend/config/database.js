9374b99af19043b8508a6ca59cf545c55109976d
Adarsh Kumar Jha <jhaadarshhr5216@gmail.com>
2026-04-07 23:46:54 +0530

    fix mongo uri final

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,14 +3,14 @@ require('dotenv').config();
 
 const dbConnect = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
         console.log('Database connected successfully');
     } catch (error) {
         console.error('Database connection failed:', error);
-        process.exit(1); // Exit the process with failure
+        process.exit(1);
     }
 }
 
